5bba4b4755b5bb10ce2c519dc0b0eaccd4cd6a46
Will Ratkowski <wlratkowski@gmail.com>
2025-11-27 12:48:25 -0700

    first

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT,
+    host: true,
+    allowedHosts: ['ejr-photography.onrender.com'] // add your Render hostname
+  }
 })
+
